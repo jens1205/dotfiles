6dc3bd047722b4bf5d06e1427dff22f2aebb81c0
Jens Gersdorf <jens.gersdorf@telekom.de>
2021-06-30 16:02:24 +0200

    show path in lightline status line

--- config/nvim/lua/lightline_config.lua
+++ config/nvim/lua/lightline_config.lua
@@ -1,6 +1,20 @@
 --Set statusbar
 -- vim.g.lightline = { colorscheme = 'onedark';
+vim.cmd([[
+function! LightlineFilename()
+  let root = fnamemodify(get(b:, 'git_dir'), ':h')
+  let path = expand('%:p')
+  if path[:len(root)-1] ==# root
+    return path[len(root)+1:]
+  endif
+  return expand('%')
+endfunction
+]])
 vim.g.lightline = { colorscheme = 'one';
       active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } };
-      component_function = { gitbranch = 'fugitive#head', };
+      component_function = {
+          gitbranch = 'fugitive#head',
+          filename = 'LightlineFilename',
+      };
 }
+
