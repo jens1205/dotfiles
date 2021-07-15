ad5d3f5afc8fdbfbe0f61e216282930e76e17e41
Jens Gersdorf <jens.gersdorf@telekom.de>
2021-07-15 09:08:54 +0200

    kitty installation fixed

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+KITTY_THEME_DIR="$HOME/.local/share/kitty-themes"
+
+if [ ! -d "$KITTY_THEME_DIR" ]; then
+    git clone --depth 1 https://github.com/dexpota/kitty-themes.git "$KITTY_THEME_DIR"
+else
+    echo "kitty themes already installed"
+fi
 
 stow -t ~ kitty
 stow -t ~ nvim
 stow -t ~ tmux
 stow -t ~ vim
 stow -t ~ zsh
-
 
--- kitty/.config/kitty/install_themes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-git clone --depth 1 git@github.com:dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
-ln -s ~/.config/kitty/kitty-themes/themes/OneDark.conf ~/.config/kitty/theme.conf
-
