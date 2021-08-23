baf985d755bbab9aeb8ccbe09e62b7e12dde23d8
Jens Gersdorf <jens.gersdorf@telekom.de>
2021-08-23 10:11:17 +0200

    disable neogit commit confirm message

--- nvim/.config/nvim/lua/config/neogit.lua
+++ nvim/.config/nvim/lua/config/neogit.lua
@@ -3,7 +3,7 @@ local neogit = require("neogit")
 neogit.setup {
   -- disable_signs = false,
   -- disable_context_highlighting = false,
-  -- disable_commit_confirmation = false,
+  disable_commit_confirmation = true,
   -- -- customize displayed signs
   -- signs = {
   --   -- { CLOSED, OPENED }
