f679f4584bc67829d6f42a826d013bd395f79d53
Jens Gersdorf <jens.gersdorf@telekom.de>
2021-09-13 09:28:39 +0200

    removed comma mapping

--- nvim/.config/nvim/lua/config/nvim-compe.lua
+++ nvim/.config/nvim/lua/config/nvim-compe.lua
@@ -83,7 +83,7 @@ _G.comma = function()
     end
 end
 
-vim.api.nvim_set_keymap("i", ",", "v:lua.comma()", { expr = true })
+-- vim.api.nvim_set_keymap("i", ",", "v:lua.comma()", { expr = true })
 vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
