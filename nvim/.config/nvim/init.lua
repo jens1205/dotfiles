d93a1a5058f5951e59ddc2e9684da7d0e89dcbcc
Jens Gersdorf <jens.gersdorf@telekom.de>
2021-09-22 15:03:30 +0200

    format json (temporary put into init.lua until we find a better place)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,10 @@ require('settings')
 require('mappings').general()
 require('dbg')
 -- require'mappings'.restnvim()
-
+vim.cmd[[
+    command FormatJson :%!jq .
+]]
 --Set colorscheme (order is important here)
 vim.o.termguicolors = true
 vim.cmd[[colorscheme onedark]]
+
