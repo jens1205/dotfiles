14f6fb9baaf3194d21c8c75628959eb0010b8ef7
Jens Gersdorf <jens.gersdorf@telekom.de>
2022-01-19 11:24:40 +0100

    added golangcilint languageserver

--- lsp.sh
+++ lsp.sh
@@ -3,6 +3,8 @@
 go install golang.org/x/tools/gopls@latest
 
 brew upgrade rust-analyzer
+brew upgrade golangci-lint
+go get github.com/nametake/golangci-lint-langserver
 
 npm install --save typescript
 npm install -g typescript-language-server
