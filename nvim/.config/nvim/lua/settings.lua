45a2160a2d6a692d1463567a4c74b922395cad85
Jens Gersdorf <jens.gersdorf@telekom.de>
2022-02-09 09:52:34 +0100

    add colorcolumn

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -4,6 +4,7 @@ vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
+vim.o.colorcolumn = "120"
 
 vim.o.termguicolors = true -- set term gui colors most terminals support this
 
