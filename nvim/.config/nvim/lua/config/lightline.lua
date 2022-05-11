9a44d2a00ba93f62fba074522df4222d6be2874f
Jens Gersdorf <jens.gersdorf@telekom.de>
2022-05-11 20:29:09 +0200

    adapt to fugitive update
    fugitive#head -? FugitiveHead

--- nvim/.config/nvim/lua/config/lightline.lua
+++ nvim/.config/nvim/lua/config/lightline.lua
@@ -14,7 +14,7 @@ vim.g.lightline = {
 	colorscheme = "one",
 	active = { left = { { "mode", "paste" }, { "gitbranch", "readonly", "filename", "modified" } } },
 	component_function = {
-		gitbranch = "fugitive#head",
+		gitbranch = "FugitiveHead",
 		filename = "LightlineFilename",
 	},
 }
