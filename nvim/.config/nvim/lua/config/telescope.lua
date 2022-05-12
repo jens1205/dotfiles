5e5036506e93820011877330625369531db5226e
Jens Gersdorf <jens.gersdorf@telekom.de>
2022-05-12 09:25:26 +0200

    add extension ui-select (used by rust-tools nvim)

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -77,6 +77,7 @@ require("telescope").setup({
 		fzy_native = { override_generic_sorter = false, override_file_sorter = true },
 	},
 })
+require("telescope").load_extension("ui-select")
 
 require("mappings").telescope()
 
