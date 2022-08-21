5bc563d8b4a9af300f4448cb3f3829651e7203b2
Jens Gersdorf <jens.gersdorf@telekom.de>
2022-08-21 21:01:12 +0200

    add lsp-lines

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -46,6 +46,16 @@ local function install()
 				})
 			end,
 		})
+		use({
+			"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+			config = function()
+				-- Disable virtual_text since it's redundant due to lsp_lines.
+				vim.diagnostic.config({
+					virtual_text = false,
+				})
+				require("lsp_lines").setup()
+			end,
+		})
 		use("ggandor/lightspeed.nvim")
 		-- use({
 		-- 	"TimUntersberger/neogit",
