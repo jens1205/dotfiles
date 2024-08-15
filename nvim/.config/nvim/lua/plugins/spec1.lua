dbc742383dff8929509747dc49e1cbcc292f0772
Jens Gersdorf <jens.gersdorf@telekom.de>
2024-08-15 08:51:33 +0200

    remove tabnine

--- nvim/.config/nvim/lua/plugins/spec1.lua
+++ nvim/.config/nvim/lua/plugins/spec1.lua
@@ -254,26 +254,6 @@ return {
 		end,
 	},
 	{
-		"tzachar/cmp-tabnine",
-		dependencies = "hrsh7th/nvim-cmp",
-		build = "./install.sh",
-		config = function()
-			local tabnine = require("cmp_tabnine.config")
-			tabnine:setup({
-				max_lines = 1000,
-				max_num_results = 20,
-				sort = true,
-				run_on_every_keystroke = true,
-				snippet_placeholder = "..",
-				ignored_file_types = { -- default is not to ignore
-					-- uncomment to ignore in lua:
-					-- lua = true
-				},
-				show_prediction_strength = false,
-			})
-		end,
-	},
-	{
 		"windwp/nvim-autopairs",
 		config = function()
 			require("config.nvim-autopairs")
